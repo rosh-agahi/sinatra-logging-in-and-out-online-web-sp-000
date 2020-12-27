d606d17d6968ad9d55411d96d95846404fdf4417
Roshanak Agahi <rosh.agahi@gmail.com>
2020-12-27 03:32:44 +0000

    Automatically backed up by Learn

--- db/migrate/20201227033045_create_users.rb
+++ db/migrate/20201227033045_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.decimal :balance, :precision => 10, :scale => 2
+      t.decimal :balance
     end
   end
 end
